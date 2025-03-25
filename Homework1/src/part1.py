bb56d6d238b61be0fc92ec73032e0312237dbc66
Berkin Yılmaz <111027197+Berkin99@users.noreply.github.com>
2025-03-25 20:17:31 +0300

    HW1 Init

---
+++ Homework1/src/part1.py
@@ -0,0 +1,6 @@
+
+# Part 1: Build a classifier based on KNN (K=3 for testing) using Euclidean distance.
+# o You are expected to code the KNN classifier (including the distance function).
+# o Report performance using an appropriate k-fold cross validation using confusion
+# matrices on the given dataset.
+# o Report the run time performance of your above tests
